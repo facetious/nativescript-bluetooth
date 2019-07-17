6bde46e90806dfb035c7ed40deaa927d2c34699b
Ian MacDonald <IanMacDonald@nanometrics.ca>
2019-07-17 14:34:38 -0400

    (fix) called the resolve of the Promise on connect.

--- src/android/android_main.ts
+++ src/android/android_main.ts
@@ -358,6 +358,8 @@ export class Bluetooth extends BluetoothCommon {
             device: gatt // TODO rename device to gatt?
           };
         }
+
+        resolve();
       } catch (ex) {
         CLog(CLogTypes.error, `Bluetooth.connect ---- error: ${ex}`);
         reject(ex);
